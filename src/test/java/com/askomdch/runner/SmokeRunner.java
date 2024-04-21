a588845392e5c87ec14ea68358d839428932b825
tetianapavlova <pavlova0409@gmail.com>
2024-04-21 16:30:01 -0400

    test7

--- src/test/java/com/askomdch/runner/SmokeRunner.java
+++ src/test/java/com/askomdch/runner/SmokeRunner.java
@@ -4,13 +4,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/java/com/askomdch/features"},
+        features = {"src/test/java/com.askomdch/features"},
         glue = {"com/askomdch/step_definitions", "com/askomdch/hooks"},
         dryRun = false,
         tags = "@smoke",
-        plugin ={"pretty", "junit:target/surefire-reports/regressionReport.xml",
-                "json:target/surefire-reports/regressionReport.json",
-                "html:target/surefire-reports/regressionReport.html"}
+        plugin={"pretty", "junit:target/surefire-reports/smokeReport.xml",
+                "json:target/surefire-reports/smokeReport.json",
+                "html:target/surefire-reports/smokeReport.html"}
 )
 public class SmokeRunner {
 
